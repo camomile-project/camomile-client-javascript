6c252c1ad0c371f5ad7eaf276cad75a19cd36844
Hervé Bredin <bredin@limsi.fr>
2018-03-14 21:08:47 +0100

    chore: remove EventSource polyfill
    see https://caniuse.com/#feat=eventsource

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 
-// Copyright (c) 2014-2015 CNRS
+// Copyright (c) 2014-2018 CNRS
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -23,8 +23,6 @@
 "use strict";
 var r=require("request");
 var rp=require("request-promise");
-var EventSource=require("eventsource");
-
 
 function _id(result) {
   if (Array.isArray(result)) {
